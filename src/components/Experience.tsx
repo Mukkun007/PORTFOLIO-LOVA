845bbe880b0ab5db76fc690228bb0f7c97e0b248
Mukkun <fitahianalova@gmail.com>
2025-03-29 12:11:13 +0300

    title xp

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -70,42 +70,49 @@ const Experience = () => {
   ];
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#0f172a] mt-10">
-      <div className="relative w-2/3 lg:w-1/2">
-        {/* Ligne verticale encore plus à gauche */}
-        <div className="absolute left-[-10px] lg:left-1/2 w-1 bg-[#122b39] h-full lg:-translate-x-1/2"></div>
-
-        {/* Expériences (cartes alignées à droite) */}
-        {experiences.map((exp, index) => (
-          <div
-            key={index}
-            className={`flex ${
-              index % 2 === 0 ? "lg:justify-start" : "lg:justify-end"
-            } justify-end my-[100px] relative lg:my-[150px] lg:relative`}
-          >
-            {/* Icône directement SUR la timeline */}
-            <div className="w-[50px] sm:w-[55px] md:w-[60px] lg:w-[70px] h-[50px] sm:h-[55px] md:h-[60px] lg:h-[70px] bg-[#122b39] rounded-full flex items-center justify-center text-white text-xl lg:text-2xl absolute left-[-33.3px]  sm:left-[-33.5px] md:left-[-36.5px] lg:left-1/2 lg:-translate-x-1/2">
-              {exp.icon}
-            </div>
-
-            {/* Contenu de l'expérience (aligné à droite) */}
-            <div
-              className={`bg-white shadow-md p-6 rounded-lg w-4/5 lg:w-2/5 ml-auto lg:ml-0 lg:relative lg:${
-                index % 2 === 0 ? "ml-24" : "mr-24"
-              }`}
-            >
-              <h3 className="text-[11px] lg:text-lg sm:text-[14px] md:text-[16px] font-semibold text-gray-700">
-                {exp.date}
-              </h3>
-              <p className="text-blue-600 font-bold text-[11px] sm:text-[20px] md:text-[22px]">
-                {exp.establishment}
-              </p>
-              <p className="text-gray-700 sm:text-[18px]">{exp.position}</p>
-            </div>
-          </div>
-        ))}
+    <>
+      <div>
+        <h1 className="font-bold text-center mt-5 lg:mt-8 xl:mt-10 text-3xl sm:text-[40px] md:text-[35px] lg:text-[40px] xl:text-3xl 2xl:text-[40px] text-white ml-[40px] mr-[40px]">
+          {t("experience.title")}
+        </h1>
       </div>
-    </div>
+      <div className="flex justify-center items-center min-h-screen bg-[#0f172a] mt-10">
+        <div className="relative w-2/3 lg:w-1/2">
+          {/* Ligne verticale encore plus à gauche */}
+          <div className="absolute left-[-10px] lg:left-1/2 w-1 bg-[#122b39] h-full lg:-translate-x-1/2"></div>
+
+          {/* Expériences (cartes alignées à droite) */}
+          {experiences.map((exp, index) => (
+            <div
+              key={index}
+              className={`flex ${
+                index % 2 === 0 ? "lg:justify-start" : "lg:justify-end"
+              } justify-end my-[100px] relative lg:my-[150px] lg:relative`}
+            >
+              {/* Icône directement SUR la timeline */}
+              <div className="w-[50px] sm:w-[55px] md:w-[60px] lg:w-[70px] h-[50px] sm:h-[55px] md:h-[60px] lg:h-[70px] bg-[#122b39] rounded-full flex items-center justify-center text-white text-xl lg:text-2xl absolute left-[-33.3px]  sm:left-[-33.5px] md:left-[-36.5px] lg:left-1/2 lg:-translate-x-1/2">
+                {exp.icon}
+              </div>
+
+              {/* Contenu de l'expérience (aligné à droite) */}
+              <div
+                className={`bg-white shadow-md p-6 rounded-lg w-4/5 lg:w-2/5 ml-auto lg:ml-0 lg:relative lg:${
+                  index % 2 === 0 ? "ml-24" : "mr-24"
+                }`}
+              >
+                <h3 className="text-[11px] lg:text-lg sm:text-[14px] md:text-[16px] font-semibold text-gray-700">
+                  {exp.date}
+                </h3>
+                <p className="text-blue-600 font-bold text-[11px] sm:text-[20px] md:text-[22px]">
+                  {exp.establishment}
+                </p>
+                <p className="text-gray-700 sm:text-[18px]">{exp.position}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
